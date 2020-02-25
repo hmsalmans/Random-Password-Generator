5b7e5a9115a99eed17dff72af1f61ea7b3f02d82
hafiz salman <hmsalmans1@gmail.com>
2020-02-25 05:26:22 -0500

    added rest of variable and deployed

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,7 @@ generateBtn.addEventListener("click", writePassword);
     
 
 let characters = prompt("Please chose between 8 and 128 characters for your passward")
-  if  (characters >= 8 && characters <= 128) {
+  if (characters >= 8 && characters <= 128) {
     alert(characters + " Characters chosen.")
   }
     else {
@@ -30,9 +30,7 @@ let caps = confirm("Would you like capital letters in your passward?")
 if (caps){
   var cAPT = (CapLetter.charAt(Math.floor(Math.random() * 26)))
 }
-else {
-  alert("Ok! Bye then.")
-}
+
 
 
 let smaLetter = "abcdefghijklmnopqrstuvwxyz"
@@ -40,9 +38,7 @@ let smas = confirm("Would you like small letters in your passward?")
 if (smas){
    var sAML = (smaLetter.charAt(Math.floor(Math.random() * 26)))
 }
-else {
-  alert("Ok! Bye then.")
-}
+
 
 
 
@@ -51,9 +47,6 @@ let speLetter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 let spac = confirm("Would you like any special characters in your passward?")
 if (spac){
 var sPEL = (speLetter.charAt(Math.floor(Math.random() * 31)))
-}
-else {
-  alert("Ok! Bye then.")
 }
 
 
@@ -64,9 +57,79 @@ let numb = confirm("Would you like any numbers in your passward?")
 if (numb){
 var nUMB = (numbers.charAt(Math.floor(Math.random() * 10)))
 }
-else {
-  alert("Ok! Bye then.")
+
+
+
+
+
+
+if (!caps && !smas && !spac  && !numb)
+alert("You did not chose any value")
+
+else if (caps && smas && spac  && numb) {
+  choice = cAPT.concat(sAML, sPEL, nUMB)
 }
+else if (caps && smas && spac) {
+  choice = cAPT.concat(sAML, sPEL)
+}
+
+
+else if (caps && smas && numb) {
+  choice = cAPT.concat(sAML, nUMB)
+}
+
+else if (caps && numb && spac) {
+  choice = cAPT.concat(nUMB, sPEL)
+}
+
+else if (numb && smas && spac) {
+  choice = nUMB.concat(sAML, sPEL)
+}
+
+
+else if (caps && smas) {
+  choice = cAPT.concat(sAML)
+}
+
+else if (caps && spac) {
+  choice = cAPT.concat(sPEL)
+}
+
+else if (caps && numb) {
+  choice = cAPT.concat(nUMB)
+}
+
+else if (spac && smas) {
+  choice = sPEL.concat(sAML)
+}
+
+else if (numb && smas) {
+  choice = nUMB.concat(sAML)
+}
+
+else if (numb && spac) {
+  choice = nUMB.concat(sPEL)
+}
+
+
+else if (numb) {
+  choice = nUMB
+}
+
+
+else if (spac) {
+  choice = sPEL
+}
+
+else if (smas) {
+  choice = sAML
+}
+
+else if (caps) {
+  choice = cAPT
+};
+
+
 
 
 
