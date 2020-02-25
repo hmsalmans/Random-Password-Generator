62adfebff092598cbb7bdade396a1b60dbb18a1b
hafiz salman <hmsalmans1@gmail.com>
2020-02-25 01:15:40 -0500

    adding passward small and cap letter variables

--- Develop/script.js
+++ Develop/script.js
@@ -13,8 +13,62 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+    
+
+let characters = prompt("Please chose between 8 and 128 characters for your passward")
+  if  (characters >= 8 && characters <= 128) {
+    alert(characters + " Characters chosen.")
+  }
+    else {
+      alert("Sorry! Criteria is not met.")
+
+  }
 let CapLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-alert(CapLetter.charAt(Math.floor(Math.random() * 26)))
+let caps = confirm("Would you like capital letters in your passward?")
+if (caps){
+  var cAPT = (CapLetter.charAt(Math.floor(Math.random() * 26)))
+}
+else {
+  alert("Ok! Bye then.")
+}
+
+
+let smaLetter = "abcdefghijklmnopqrstuvwxyz"
+let smas = confirm("Would you like small letters in your passward?")
+if (smas){
+   alert (smaLetter.charAt(Math.floor(Math.random() * 26)))
+}
+else {
+  alert("Ok! Bye then.")
+}
+
+
+
+
+let smaLetter = "abcdefghijklmnopqrstuvwxyz"
+let smas = confirm("Would you like small letters in your passward?")
+if (smas){
+   alert (smaLetter.charAt(Math.floor(Math.random() * 26)))
+}
+else {
+  alert("Ok! Bye then.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
