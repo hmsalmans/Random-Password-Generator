f7b6cb8e129d3befb2fdb683916eacc41a145d98
hafiz salman <hmsalmans1@gmail.com>
2020-02-26 23:00:59 -0500

    Generated randome passward

--- Develop/script.js
+++ Develop/script.js
@@ -23,12 +23,17 @@ let characters = prompt("Please chose between 8 and 128 characters for your pass
       alert("Sorry! Criteria is not met.")
 }
 
-
+var mene = "";
+var suerpStr = "";
+var count = 0;
 
 let CapLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let caps = confirm("Would you like capital letters in your passward?")
-if (caps){
+if (caps) {
   var cAPT = (CapLetter.charAt(Math.floor(Math.random() * 26)))
+  suerpStr = suerpStr + CapLetter;
+  mene = mene + cAPT;
+  count++;
 }
 
 
@@ -37,6 +42,9 @@ let smaLetter = "abcdefghijklmnopqrstuvwxyz"
 let smas = confirm("Would you like small letters in your passward?")
 if (smas){
    var sAML = (smaLetter.charAt(Math.floor(Math.random() * 26)))
+suerpStr = suerpStr + smaLetter; 
+mene = mene + sAML
+count++
 }
 
 
@@ -47,6 +55,9 @@ let speLetter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 let spac = confirm("Would you like any special characters in your passward?")
 if (spac){
 var sPEL = (speLetter.charAt(Math.floor(Math.random() * 31)))
+suerpStr = suerpStr + speLetter
+mene = mene + sPEL
+count++
 }
 
 
@@ -56,95 +67,17 @@ let numbers = "1234567890"
 let numb = confirm("Would you like any numbers in your passward?")
 if (numb){
 var nUMB = (numbers.charAt(Math.floor(Math.random() * 10)))
+  suerpStr = suerpStr + numbers
+  mene = mene + nUMB;
+  count++;
+
 }
 
+var charLength = parseInt(characters);
 
-
-
-
-
-if (!caps && !smas && !spac  && !numb)
-alert("You did not chose any value")
-
-else if (caps && smas && spac  && numb) {
-  choice = cAPT.concat(sAML, sPEL, nUMB)
-}
-else if (caps && smas && spac) {
-  choice = cAPT.concat(sAML, sPEL)
+for (var j = 0; j < charLength-count; j++) {
+  var randomChar = suerpStr.charAt(Math.floor(Math.random()*suerpStr.length));
+  mene = mene + randomChar;
 }
 
-
-else if (caps && smas && numb) {
-  choice = cAPT.concat(sAML, nUMB)
-}
-
-else if (caps && numb && spac) {
-  choice = cAPT.concat(nUMB, sPEL)
-}
-
-else if (numb && smas && spac) {
-  choice = nUMB.concat(sAML, sPEL)
-}
-
-
-else if (caps && smas) {
-  choice = cAPT.concat(sAML)
-}
-
-else if (caps && spac) {
-  choice = cAPT.concat(sPEL)
-}
-
-else if (caps && numb) {
-  choice = cAPT.concat(nUMB)
-}
-
-else if (spac && smas) {
-  choice = sPEL.concat(sAML)
-}
-
-else if (numb && smas) {
-  choice = nUMB.concat(sAML)
-}
-
-else if (numb && spac) {
-  choice = nUMB.concat(sPEL)
-}
-
-
-else if (numb) {
-  choice = nUMB
-}
-
-
-else if (spac) {
-  choice = sPEL
-}
-
-else if (smas) {
-  choice = sAML
-}
-
-else if (caps) {
-  choice = cAPT
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+alert(mene);
